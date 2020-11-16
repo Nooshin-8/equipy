d8cdbe77b41409a9dca93a1ed3ebbf4d24b362c8
Nooshin Attarian <noshinatarian@gmail.com>
2020-11-16 14:53:37 -0500

    finished the routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :equipment do
+    resources :bookings, only: [:create]
+  end
+
+  resources :bookings, only: [:index, :update, :destroy]
+
+  # you will see the result of your search for a particular equipment
+  get '/search_results', to: 'search_results#index'
 end
