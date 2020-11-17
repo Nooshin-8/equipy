3275379c9232a45cba02f31a739b2e9d82f42c02
Nawel Aammar <nawel.aa@hotmail.fr>
2020-11-17 10:57:32 -0500

    Bookings: create action and partial for new form

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,18 @@
 class BookingsController < ApplicationController
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    @booking.equipment = Equipment.find(params[:equipment_id])
+    @booking.accepted = "pending"
+
+    @booking.save
+
+    redirect_to profile_path
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date)
+  end
 end
