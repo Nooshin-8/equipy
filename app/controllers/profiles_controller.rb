abe5ca997d0fcd1c8465231ad35deed14a7ebe47
Tatiana Labrousse <mvtatianalabrousse@gmail.com>
2020-11-17 12:15:27 -0500

    added actions to profile controller

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,27 @@
+class ProfilesController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to edit_user_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_user
+    @user = current_user
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :profile_picture)
+  end
+end
