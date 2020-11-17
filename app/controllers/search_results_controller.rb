14f378a554b8d1c6c2ca831edfede5be12d4884a
Nawel Aammar <nawel.aa@hotmail.fr>
2020-11-17 17:40:43 -0500

    Search results page created, basic result card

--- app/controllers/search_results_controller.rb
+++ app/controllers/search_results_controller.rb
@@ -1,2 +1,7 @@
 class SearchResultsController < ApplicationController
+  def index
+    query = params[:query].downcase
+    @results = policy_scope(Equipment)
+    @results = Equipment.where("lower(title) LIKE ? OR lower(description) LIKE ?", "%#{query}%", "%#{query}%")
+  end
 end
