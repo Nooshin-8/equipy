9a0cdf3415b95c0e124ff3572a2b9a93d9c89404
Nawel Aammar <nawel.aa@hotmail.fr>
2020-11-18 11:25:58 -0500

    Profile show action sets variables @my_bookings and @my_equipment_booked

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,6 +2,14 @@ class ProfilesController < ApplicationController
   before_action :set_user, only: %i[show edit update]
 
   def show
+    # Bookings the current user made
+    @my_bookings = Booking.where(user: current_user)
+
+    # Bookings people made on current user's equipment
+    all_equipment = Equipment.where(user: current_user)
+    @my_equipment_booked = all_equipment.map do |equipment|
+      Booking.where(equipment_id: equipment.id)
+    end
   end
 
   def edit
