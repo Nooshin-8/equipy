80d63778b3bdc3e8c7e797ba05d07e413cf15ad1
Nooshin Attarian <noshinatarian@gmail.com>
2020-11-19 17:05:06 -0500

    linked the categories to search result page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2020_11_19_190234) do
 
   # These are extensions that must be enabled in order to support this database
@@ -63,6 +62,14 @@ ActiveRecord::Schema.define(version: 2020_11_19_190234) do
     t.index ["user_id"], name: "index_equipment_on_user_id"
   end
 
+  create_table "pg_search_documents", force: :cascade do |t|
+    t.text "content"
+    t.string "searchable_type"
+    t.bigint "searchable_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable_type_and_searchable_id"
+  end
 
   create_table "reviews", force: :cascade do |t|
     t.string "title"
@@ -74,14 +81,6 @@ ActiveRecord::Schema.define(version: 2020_11_19_190234) do
     t.bigint "user_id", null: false
     t.index ["equipment_id"], name: "index_reviews_on_equipment_id"
     t.index ["user_id"], name: "index_reviews_on_user_id"
-
-  create_table "pg_search_documents", force: :cascade do |t|
-    t.text "content"
-    t.string "searchable_type"
-    t.bigint "searchable_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable_type_and_searchable_id"
   end
 
   create_table "users", force: :cascade do |t|
