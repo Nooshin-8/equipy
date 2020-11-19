8c5e3361e32d9c72cc27c72b5cc93d975e535d84
Nooshin Attarian <noshinatarian@gmail.com>
2020-11-19 14:56:05 -0500

    applied the pg search to equipment title and category

--- app/models/equipment.rb
+++ app/models/equipment.rb
@@ -8,4 +8,18 @@ class Equipment < ApplicationRecord
 
   geocoded_by :location
   after_validation :geocode, if: :will_save_change_to_location?
+
+  # include PgSearch::Model
+  # multisearchable against: [:title]
+
+  include PgSearch::Model
+  pg_search_scope :main_search, # the name of the method you need to call
+  # in the controller when you want to search
+    against: [ :title, :category], # it will search by title of the equipment
+
+    using: {
+      tsearch: { prefix: true } # <-- now `Basket b` will return something!
+    }
+    # tsearch is equivalant of @@
+    # we enable prefix which means partial words are allowed, but only the begining of the word
 end
