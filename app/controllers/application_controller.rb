310810675f4be8b34194fe28b641b36ba192b34c
Nawel Aammar <nawel.aa@hotmail.fr>
2020-11-20 11:50:37 -0500

    notification bubble on top of avatar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  before_action :authenticate_user!, :set_user_data
 
   include Pundit
 
@@ -18,5 +18,25 @@ class ApplicationController < ActionController::Base
 
   def skip_pundit?
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
+  end
+
+  def set_user_data
+    @user = current_user
+
+    # Get all the user's equipment
+    @my_equipment = Equipment.where(user: @user)
+
+    # Bookings people made on current user's equipment
+    ## For each equipment, get the pending booking(s) associated
+    ## => Returns an array containing one entry per equipment.
+    @my_equipment_bookings = @my_equipment.map do |equipment|
+      Booking.where(equipment: equipment).where(accepted: 'pending')
+    end
+
+    ## Removes the empty entries coming from equipment without bookings.
+    ## => Returns an array of arrays [[booking1, booking2], [booking]]
+    @my_equipment_bookings.reject!(&:empty?)
+
+    @pending_requests_count = @my_equipment_bookings.sum(&:count)
   end
 end
--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,23 +3,7 @@ class ProfilesController < ApplicationController
 
   def show
     # Bookings the current user made
-    @my_bookings = Booking.where(user: current_user)
-
-    # Get all the user's equipment
-    @my_equipment = Equipment.where(user: current_user)
-
-    # Bookings people made on current user's equipment
-    ## For each equipment, get the pending booking(s) associated
-    ## => Returns an array containing one entry per equipment.
-    @my_equipment_bookings = @my_equipment.map do |equipment|
-      Booking.where(equipment: equipment).where(accepted: 'pending')
-    end
-
-    ## Removes the empty entries coming from equipment without bookings.
-    ## => Returns an array of arrays [[booking1, booking2], [booking]]
-    @my_equipment_bookings.reject!(&:empty?)
-
-    @pending_requests_count = @my_equipment_bookings.sum(&:count)
+    @my_bookings = Booking.where(user: @user)
   end
 
   def edit
